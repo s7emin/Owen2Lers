0c9ef8b2d10a157f48a0d31da12b9a5b0d635074
s7emin <s@cemin.ru>
2025-01-31 05:02:39 +0500

    добавил вывод параметров OwenCloud

--- setup.py
+++ setup.py
@@ -36,6 +36,17 @@ def get_devices(token):
     else:
         raise Exception(f"Ошибка при получении списка приборов: {response.status_code}, {response.text}")
 
+def get_device_parameters(token, device_id):
+    device_url = f"https://api.owencloud.ru/v1/device/{device_id}"
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.post(device_url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("parameters", []), data.get("parameter_categories", [])
+    else:
+        raise Exception(f"Ошибка при получении параметров прибора {device_id}: {response.status_code}, {response.text}")
+
 def test_lers_connection(server_url):
     try:
         response = requests.get(f"{server_url}/api/v1/ServerInfo")
@@ -79,7 +90,15 @@ def create_config():
             print(f"{Colors.GREEN}Список доступных приборов:{Colors.RESET}")
             for device in devices:
                 print(f"{Colors.GREEN}{device['name']}{Colors.RESET}")
-            break  # Выход из цикла при успешном получении списка приборов
+                parameters, categories = get_device_parameters(config['owen_token'], device['id'])
+                #print(f"{Colors.GREEN}Параметры для прибора {device['name']}:{Colors.RESET}")
+                for category in categories:
+                    print(f"{Colors.GREEN}Категория: {category['name']}{Colors.RESET}")
+                    for param in parameters:
+                        if param['category_id'] == category['id']:
+                            measurement_title = param.get('measurement', {}).get('title', '')
+                            print(f"  {param['name']} (ID: {Colors.GREEN}{param['id']}{Colors.RESET}), Значение: {param['formatted_value']} {measurement_title}")
+            break  # Выход из цикла при успешном получении списка приборов и параметров
         except Exception as e:
             print(f"{Colors.RED}Ошибка при получении списка приборов: {e}. Попробуйте снова.{Colors.RESET}")
 
